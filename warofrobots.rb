b887976d71b1f7af2cfbc51ae93665dbd44d9020
Anton Sav <savitskiy34@gmail.com>
2020-10-26 16:58:47 +0200

    new war

--- warofrobots.rb
+++ warofrobots.rb
@@ -1,58 +1,66 @@
-puts "hello Anton"
 
 
-comandaOne=Array.new(10,1)
-comandaTwo=Array.new(10,1)
+@arr1 = Array.new(10,1)
+@arr2 = Array.new(10,1)
 
 
-#стреляет первая команда
+def attack(arr)
+
+  i = rand(0..9)
+   if arr[i] == 1
+      arr[i] = 0
+      puts "робот под номером #{ i+1 } уничтожен"
+   else
+     puts "Промазал по роботу #{ i+1 } "
+  end
+
+end
+
+
+def victory?
+  robots_left1 = @arr1.count {|x| x == 1}
+  robots_left2 = @arr2.count {|x| x == 1}
+ if robots_left1 == 0
+   puts "Команда 2 победила , в команде осталось #{robots_left2} роботов "
+   return true
+end
+if robots_left2 == 0
+  puts "Команда 1 победила , в команде осталось #{robots_left1} роботов "
+  return false
+end
+false
+end
+
+def stats (x)
+  cnt1 = @arr1.count {|x| x == 1}
+  cnt2 = @arr2.count {|x| x == 1}
+  if x == 1
+  puts "Первая команда: #{cnt1} роботов в строю"
+end
+if x == 2
+  puts "Вторая команда: #{cnt2} роботов в строю"
+end
+end
+
+
+#########################################################
+# j=0
 loop do
 
-shot = rand(0..9)
-puts "стреляет первая команда"
-if comandaOne[shot] == 1
-   comandaOne[shot] = 0
-   puts "робот под номером #{shot+1} убит "
-else
-  puts "Промaзали"
-end
+  #  j+=1
+  # exit if j == 2
 
-print comandaOne
+puts "первая команда наносит удар"
+attack(@arr2)
+exit if victory?
+stats (1)
+puts "осталось роботов в первой команде #{@arr1}"
+puts""
+
+puts "вторая команда наносит удар"
+attack(@arr1)
+exit if victory?
+stats (2)
+puts "осталось роботов во второй команде #{@arr2}"
 puts ""
-
-
-#считаем количество выживших
-x=comandaOne.count{|x| x==1}
-puts "осталось #{x} роботов в первой команде"
-puts ""
-#стреляет вторая команда
-shot = rand(0..9)
-puts "стреляет вторая команда"
-if comandaTwo[shot] == 1
-   comandaTwo[shot] = 0
-   puts "робот под номером #{shot+1} убит "
-else
-  puts "Промaзали"
-end
-
-print comandaTwo
-puts ""
-
-
-
-#считаем количество выживших
-y=comandaTwo.count{|y| y==1}
-puts "осталось #{x} роботов во второ команде"
-puts ""
-
-case
-
-when x == 0 || x == 0 && y == 1
-  puts "выиграла вторая команда"
-  return
-when
-     y == 0 || y == 0 && x == 1
-     puts "выиграла первая команда"
-     return
-end
 end
